26635d4736429d1e7af7ca73860b045a70b66d39
MRITYUNJAY UPADHYAY <43280133+upadhyay01@users.noreply.github.com>
2023-09-20 00:01:03 +0530

    Add files via upload

---
+++ pract.py
@@ -0,0 +1,8 @@
+t=['something about that. [...] We protested at football games, and had some',
+ '',
+ 'meetings with the administration, etc. Our demands were a little',
+ '']
+
+
+for k in t:
+    print(k)
